b41224790219c83e9dc7eede10a430d70db40ca8
Krystal Developer <krys_dev@Krystals-MacBook-Pro.local>
2020-01-25 17:15:59 -0500

    Completed calculator: It now takes my age in human years and converts it to what my age would be in dog years by calculating the result of earlyYears and laterYears added together.

--- 02_Dog_Age_Calculator.js
+++ 02_Dog_Age_Calculator.js
@@ -13,3 +13,12 @@ let laterYears = myAge - 2;
 laterYears *= 4; 
 // // verify laterYears value
 // console.log(laterYears);
+
+// Converter: Changes my Age in human years to dog years
+myAgeInDogYears = earlyYears + laterYears;
+
+// created a variable to add my name
+const myName = 'Krystal'.toLowerCase();
+
+// Print conversion
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
