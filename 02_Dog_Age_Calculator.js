6a82c0d416be7126949ab129e2046e5c1af9000d
Krystal Developer <krys_dev@Krystals-MacBook-Pro.local>
2020-01-25 17:01:23 -0500

    Created variables earlyYears and laterYears to calculate the difference of speed dogs age as puppies vs as adults

--- 02_Dog_Age_Calculator.js
+++ 02_Dog_Age_Calculator.js
@@ -1,2 +1,15 @@
 // created a variable named myAge to set it equal to my age as a number
 let myAge = 29;
+
+// created a variable named earlyYears and saved the value 2 to it, multiplied the value by 10.5 
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// // verify earlyYears value
+// console.log(earlyYears);
+
+// created a variable named laterYears, multiplied variable by 4 to calculate dog years
+let laterYears = myAge - 2;
+laterYears *= 4; 
+// // verify laterYears value
+// console.log(laterYears);
