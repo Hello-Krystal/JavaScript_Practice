a061a4f8aab8ff359374c49e8618a5efdd993294
Krystal Developer <krys_dev@Krystals-MacBook-Pro.local>
2020-01-25 23:16:57 -0500

    Added a control flow statement that takes in a randomNumber and assigns the result to the empty eightBall variable.

--- 03_Magic_Eight_Ball.js
+++ 03_Magic_Eight_Ball.js
@@ -14,3 +14,35 @@ randomNumber = Math.floor(Math.random() * 8);
 
 // Test random number generator
 console.log(randomNumber);
+
+// Create a switch - control flow statement that takes in a randomNumber and assigns the empty variable a reply for the Magic Eight Ball to return 
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+// prints Magic Eight Ball answer
+console.log(eightBall);
