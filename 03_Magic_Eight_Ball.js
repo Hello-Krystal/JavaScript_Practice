f6c2e5a7cb81cb620cb57d5fea288fb38add4c76
Krystal Developer <krys_dev@Krystals-MacBook-Pro.local>
2020-01-25 22:37:21 -0500

    Created an empty string userName for user input. Added a ternary expression that adds greeting

---
+++ 03_Magic_Eight_Ball.js
@@ -0,0 +1,5 @@
+// set userName to an empty string
+let userName = '';
+
+// created a ternary express that decides what to do if the user enters a name or not. 
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello, stranger!');
