75b8656cfd77fbadb1fd3d14fced2dfaa9c2b676
Krystal Developer <krys_dev@Krystals-MacBook-Pro.local>
2020-01-25 22:50:38 -0500

    Created user question variable that logs userName and userQuestion to terminal.

--- 03_Magic_Eight_Ball.js
+++ 03_Magic_Eight_Ball.js
@@ -1,5 +1,9 @@
 // set userName to an empty string
-let userName = '';
+let userName = 'Krys';
 
-// created a ternary express that decides what to do if the user enters a name or not. 
-userName ? console.log(`Hello, ${userName}!`) : console.log('Hello, stranger!');
+// ternary express that decides what to do if the user enters a name or not. 
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello, stranger! Tell me your name!');
+
+// variable for user question
+let userQuestion = "'Is daily practice important?'";
+console.log(`${userName} just asked ${userQuestion}`);
