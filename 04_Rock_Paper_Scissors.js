da6d662b53dc351b7d87044a04d96fff67169078
Krystal Developer <krys_dev@Krystals-MBP.lan>
2020-01-27 15:03:20 -0500

    Created an if statement of userInput

--- 04_Rock_Paper_Scissors.js
+++ 04_Rock_Paper_Scissors.js
@@ -1,4 +1,14 @@
 // create function getUserChoice that takes in user choice of rock, paper, or scissors
 const getUserChoice = userInput => {
+  
+    // users can pass in a parameter with different capitalizations
     userInput = userInput.toLowerCase();
+    
+    // if statement of userInput
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+      console.log(`The user has selected ${userInput}`);
+    } else {
+      console.log('Error: You must select either Rock, Paper, or Scissors!')
+    }
+    
   };
