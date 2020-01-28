4d8b86ab0692a4dfc1bb6f8a311fa91a2dcc71da
Krystal Developer <krystal.developer@gmail.com>
2020-01-28 12:50:44 -0500

    Created a function to generate a random event

--- 06_Training_Days.js
+++ 06_Training_Days.js
@@ -1,1 +1,22 @@
-// As a seasoned athlete, one of your favorite activities is running marathons. You use a service called Training Days that sends you a message for the event you signed up for and the days you have left to train
+// As a athlete, one of your favorite activities is running. 
+// Below is an imaginary service called Training Days 
+// it sends you a message for the event you signed up for and the days you have left to train
+
+// create global scope variable(s)
+const name = 'Hello-Krystal';
+
+
+// creates a event generator function
+const getRandEvent = () => {
+    const random = Math.floor(Math.random() * 3);
+    if (random === 0) {
+        return '3k Race!';
+    } else if (random === 1) {
+        return '5k Race';
+    } else if (random === 2) {
+        return 'Marathon';
+    }
+};
+
+// Test case
+console.log(getRandEvent());
