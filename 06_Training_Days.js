ab4b2ada1534b5cf8a7e4cb13952d2cbf9b0f644
Krystal Developer <krystal.developer@gmail.com>
2020-01-28 13:16:42 -0500

    Completed changes to Training_Days, added two small functions of logEvent and logTime that pulls in the parameters of name and event for logEvent and name and days for logTime. After successfully logging the correct values to the console, I interpolated a message to print my name, what event i signed up for and how many days I have left to train for said event.

--- 06_Training_Days.js
+++ 06_Training_Days.js
@@ -5,12 +5,11 @@
 // create global scope variable(s)
 const name = 'Hello-Krystal';
 
-
 // creates a event generator function
 const getRandEvent = () => {
     const random = Math.floor(Math.random() * 3);
     if (random === 0) {
-        return '3k Race!';
+        return '3k Race';
     } else if (random === 1) {
         return '5k Race';
     } else if (random === 2) {
@@ -18,5 +17,38 @@ const getRandEvent = () => {
     }
 };
 
-// Test case
-console.log(getRandEvent());
+// created getTrainingDays function to pull in an event and count the amount of days it takes to train
+const getTrainingDays = event => {
+    
+    // created empty days varialble
+    let days;
+
+    // if statement of getRandEvent. If event is x then train y days
+    if (event === '3k Race') {
+        days = 50;
+    } else if (event === '5k Race') {
+        days = 100;
+    } else if (event === 'Marathon') {
+        days = 200;
+    }
+    return days;
+};
+
+const logEvent = (name, event) => {
+    console.log(`Congratulations ${name}, you've successfully registered for the following event: ${event}!`);
+};
+
+const logTime = (name, days) => {
+    console.log(`${name}'s time to train is ${days} days.`);
+};
+
+
+// Create event variable that will be used as a parameter for logEvent
+const event = getRandEvent();
+
+// create a variable for getTrainingDays to save days as parameter for logTime
+const days = getTrainingDays(event);
+
+
+logEvent(name, event);
+logTime(name, days);
