f9eff4ccb7244f66925eaa6931a5409e467e67c3
Krystal Developer <krystal.developer@gmail.com>
2020-01-29 18:57:43 -0500

    Initial commit of Secret_Message - a practice project for arrays and methods

---
+++ 07_Secret_Message.js
@@ -0,0 +1,5 @@
+// The goal of this project is to practice arrays and methods 
+// it'll transform 'secretMessage' into a quote about coding.
+
+// original message
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
