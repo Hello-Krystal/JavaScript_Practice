468eb09d7f891d423443e64d321fc338495ce78b
Krystal Developer <krystal.developer@gmail.com>
2020-01-29 19:12:28 -0500

    Changed Secret Message by using the following methods: pop, push, shift and unshift

--- 07_Secret_Message.js
+++ 07_Secret_Message.js
@@ -3,3 +3,28 @@
 
 // original message
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+// saves the original length of the array
+let length = secretMessage.length;
+
+
+// removes the last string of the array 'JavaScript'
+secretMessage.pop();
+
+console.log('The new length of the array is now ' + (length - secretMessage.length) + ' shorter.')
+
+// add the words 'to' and 'program' to the end of the array
+secretMessage.push('to', 'program');
+console.log(secretMessage);
+
+// changes the word 'easily' to 'right'
+secretMessage[7] = 'right';
+console.log(secretMessage);
+
+// removes the first string of the array 'Learning'
+secretMessage.shift();
+console.log(secretMessage);
+
+// Makes the first string of the array 'Programming'
+secretMessage.unshift('Programming')
+console.log(secretMessage);
