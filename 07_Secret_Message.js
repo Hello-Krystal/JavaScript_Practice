0c660c6442f6c68198b28beb2650c3066438c666
Krystal Developer <krystal.developer@gmail.com>
2020-01-29 19:23:42 -0500

    Completed secret message: Programming is not about what you know it is about what you can figure out.

--- 07_Secret_Message.js
+++ 07_Secret_Message.js
@@ -7,24 +7,27 @@ let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'ea
 // saves the original length of the array
 let length = secretMessage.length;
 
-
 // removes the last string of the array 'JavaScript'
 secretMessage.pop();
 
-console.log('The new length of the array is now ' + (length - secretMessage.length) + ' shorter.')
-
 // add the words 'to' and 'program' to the end of the array
 secretMessage.push('to', 'program');
-console.log(secretMessage);
 
 // changes the word 'easily' to 'right'
 secretMessage[7] = 'right';
-console.log(secretMessage);
 
 // removes the first string of the array 'Learning'
 secretMessage.shift();
-console.log(secretMessage);
 
 // Makes the first string of the array 'Programming'
 secretMessage.unshift('Programming')
-console.log(secretMessage);
+
+// removes strrings 'get', 'right', 'the', 'first', 'time' and replace them with the word 'know'
+secretMessage.splice(6, 5, 'know');
+
+// makes the message print as a single sentence
+console.log(secretMessage.join(' '));
+
+const newLength = secretMessage.length;
+
+console.log('The original Secret Message was ' + length + ' words. The new length of the Secret Message is ' + newLength + ' words.');
